62bdf9dc3bb3fec3947f0b22d19f8ada0ee8765a
Komenpurity <jepkorirpurity8@gmail.com>
2023-03-31 17:10:16 +0300

    display first movie details

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded",() => {
+/* document.addEventListener("DOMContentLoaded",() => {
     fetch("http://localhost:3000/films")
     .then((response) => response.json())
     .then(data => {
@@ -6,3 +6,40 @@ document.addEventListener("DOMContentLoaded",() => {
     })
 })
 
+ */
+
+function dataId(id){
+    fetch(`http://localhost:3000/films/${id}`) 
+    .then((response) => response.json())
+    .then(data => {
+        console.log(data)
+        displayMovieDetails(data) 
+    })
+}
+dataId(1) 
+
+
+let movieDetails = document.getElementById("movie-details")
+
+//See first movie details 
+function displayMovieDetails(data){
+    data.forEach(movie => {
+         movieDetails.innerHTML += `
+        <div data-id = ${movie.id} class="card" id="display-details"> 
+            <img class="card-img-top" >${movie.poster}</img> 
+            <div class="card-body">
+            <h5 class="card-title">${movie.title}</h5> 
+            <li class="card-text">${movie.runtime}</li>   
+            <li>${movie.showtime}</li> 
+            <li>${movie.title}</li> 
+            </div>
+           </div>
+           `
+        let display = document.getElementById("display-details")
+        movieDetails.append(display) 
+        console.log(movieDetails); 
+    }); 
+
+    
+}
+ 
