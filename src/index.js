2e651132c153e92a0a9c7c50d121402749ec4037
Komenpurity <jepkorirpurity8@gmail.com>
2023-03-31 17:21:45 +0300

    edited into menu movie details

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
-/* document.addEventListener("DOMContentLoaded",() => {
+document.addEventListener("DOMContentLoaded",() => {
     fetch("http://localhost:3000/films")
     .then((response) => response.json())
     .then(data => {
         console.log(data) 
+        menuMovieDetails(data) 
     })
 })
 
- */
 
-function dataId(id){
+
+/* function dataId(id){
     fetch(`http://localhost:3000/films/${id}`) 
     .then((response) => response.json())
     .then(data => {
@@ -17,25 +18,23 @@ function dataId(id){
     })
 }
 dataId(1) 
+ */
 
+let movieDetails = document.getElementById("movie-menu") 
 
-let movieDetails = document.getElementById("movie-details")
-
-//See first movie details 
-function displayMovieDetails(data){
+//display all menu movie details 
+function menuMovieDetails(data){
     data.forEach(movie => {
          movieDetails.innerHTML += `
-        <div data-id = ${movie.id} class="card" id="display-details"> 
-            <img class="card-img-top" >${movie.poster}</img> 
-            <div class="card-body">
+        <ul data-id = ${movie.id} id="films"> 
+            <img  >${movie.poster}</img> 
             <h5 class="card-title">${movie.title}</h5> 
             <li class="card-text">${movie.runtime}</li>   
             <li>${movie.showtime}</li> 
             <li>${movie.title}</li> 
-            </div>
-           </div>
+           </ul>
            `
-        let display = document.getElementById("display-details")
+        let display = document.getElementById("films") 
         movieDetails.append(display) 
         console.log(movieDetails); 
     }); 
